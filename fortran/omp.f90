03017734326ca8c8d9fe9cf38dc6c14f88f78276
Dan Dawson <dandawson@purdue.edu>
2018-11-02 16:37:55 -0400

    Added build script for Rice and an example notebook

--- fortran/omp.f90
+++ fortran/omp.f90
@@ -1,12 +1,19 @@
-
+# 1 "ompgen.F90"
 MODULE omp_constants
+
+    USE omp_lib
+
     INTEGER, PARAMETER :: fomp_sched_kind = 4
-    INTEGER, PARAMETER :: fomp_lock_kind = 4
+    INTEGER, PARAMETER :: fomp_lock_kind = 8
     INTEGER, PARAMETER :: fomp_nest_lock_kind = 8
-    INTEGER(KIND=fomp_sched_kind), PARAMETER :: fomp_sched_static = 1
-    INTEGER(KIND=fomp_sched_kind), PARAMETER :: fomp_sched_dynamic = 2
-    INTEGER(KIND=fomp_sched_kind), PARAMETER :: fomp_sched_guided = 3
-    INTEGER(KIND=fomp_sched_kind), PARAMETER :: fomp_sched_auto = 4
+    INTEGER(KIND=4), PARAMETER :: fomp_sched_static = 1
+    INTEGER(KIND=4), PARAMETER :: fomp_sched_dynamic = 2
+    INTEGER(KIND=4), PARAMETER :: fomp_sched_guided = 3
+    INTEGER(KIND=4), PARAMETER :: fomp_sched_auto = 4
+
+# 21
+
+
 END MODULE omp_constants
 
 
@@ -14,451 +21,650 @@ FUNCTION fomp_enabled()
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     LOGICAL :: fomp_enabled
 
-    fomp_enabled = .FALSE.
+
+    fomp_enabled = .TRUE.
+# 38
+
 
 END FUNCTION fomp_enabled
 
 
 SUBROUTINE fomp_set_num_threads(num_threads)
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER, INTENT(IN) :: num_threads
-    IF (.FALSE.) PRINT *, num_threads
+
+
+    CALL omp_set_num_threads(num_threads)
+# 58
+
+
 
 END SUBROUTINE fomp_set_num_threads
 
 
 FUNCTION fomp_get_num_threads()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER :: fomp_get_num_threads
 
-    fomp_get_num_threads = -1
+
+    fomp_get_num_threads = omp_get_num_threads()
+# 79
+
 
 END FUNCTION fomp_get_num_threads
 
 
 FUNCTION fomp_get_max_threads()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER :: fomp_get_max_threads
 
-    fomp_get_max_threads = -1
+
+    fomp_get_max_threads = omp_get_max_threads()
+# 99
+
 
 END FUNCTION fomp_get_max_threads
 
 
 FUNCTION fomp_get_thread_num()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER :: fomp_get_thread_num
 
-    fomp_get_thread_num = -1
+
+    fomp_get_thread_num = omp_get_thread_num()
+# 119
+
 
 END FUNCTION fomp_get_thread_num
 
 
 FUNCTION fomp_get_num_procs()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER :: fomp_get_num_procs
 
-    fomp_get_num_procs = -1
+
+    fomp_get_num_procs = omp_get_num_procs()
+# 139
+
 
 END FUNCTION fomp_get_num_procs
 
 
 FUNCTION fomp_in_parallel()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     LOGICAL :: fomp_in_parallel
 
-    fomp_in_parallel = .FALSE.
+
+    fomp_in_parallel = omp_in_parallel()
+# 159
+
 
 END FUNCTION fomp_in_parallel
 
 
 SUBROUTINE fomp_set_dynamic(dynamic_threads)
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     LOGICAL, INTENT(IN) :: dynamic_threads
-    IF (.FALSE.) PRINT *, dynamic_threads
+
+
+    CALL omp_set_dynamic(dynamic_threads)
+# 179
+
 
 END SUBROUTINE fomp_set_dynamic
 
 
 FUNCTION fomp_get_dynamic()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     LOGICAL :: fomp_get_dynamic
 
-    fomp_get_dynamic = .FALSE.
+
+    fomp_get_dynamic = omp_get_dynamic()
+# 199
+
 
 END FUNCTION fomp_get_dynamic
 
 
 SUBROUTINE fomp_set_nested(nested)
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     LOGICAL, INTENT(IN) :: nested
-    IF (.FALSE.) PRINT *, nested
+
+
+    CALL omp_set_nested(nested)
+# 219
+
 
 END SUBROUTINE fomp_set_nested
 
 
 FUNCTION fomp_get_nested()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     LOGICAL :: fomp_get_nested
 
-    fomp_get_nested = .FALSE.
+
+    fomp_get_nested = omp_get_nested()
+# 239
+
 
 END FUNCTION fomp_get_nested
 
 
 SUBROUTINE fomp_set_schedule(kind, modifier)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_sched_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_sched_kind), INTENT(IN) :: kind
     INTEGER, INTENT(IN) :: modifier
-    IF (.FALSE.) PRINT *, kind, modifier
+
+
+    CALL omp_set_schedule(kind, modifier)
+# 261
+
 
 END SUBROUTINE fomp_set_schedule
 
 
 SUBROUTINE fomp_get_schedule(kind, modifier)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_sched_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_sched_kind), INTENT(OUT) :: kind
     INTEGER, INTENT(OUT) :: modifier
 
-    kind = -1
-    modifier = -1
+
+    CALL omp_get_schedule(kind, modifier)
+# 284
+
 
 END SUBROUTINE fomp_get_schedule
 
 
 FUNCTION fomp_get_thread_limit()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER :: fomp_get_thread_limit
 
-    fomp_get_thread_limit = -1
+
+    fomp_get_thread_limit = omp_get_thread_limit()
+# 304
+
 
 END FUNCTION fomp_get_thread_limit
 
 
 SUBROUTINE fomp_set_max_active_levels(max_levels)
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER, INTENT(IN) :: max_levels
-    IF (.FALSE.) PRINT *, max_levels
+
+
+    CALL omp_set_max_active_levels(max_levels)
+# 324
+
 
 END SUBROUTINE fomp_set_max_active_levels
 
 
 FUNCTION fomp_get_max_active_levels()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER :: fomp_get_max_active_levels
 
-    fomp_get_max_active_levels = -1
+
+    fomp_get_max_active_levels = omp_get_max_active_levels()
+# 344
+
 
 END FUNCTION fomp_get_max_active_levels
 
 
 FUNCTION fomp_get_level()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER :: fomp_get_level
 
-    fomp_get_level = -1
+
+    fomp_get_level = omp_get_level()
+# 364
+
 
 END FUNCTION fomp_get_level
 
 
 FUNCTION fomp_get_ancestor_thread_num(level)
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER, INTENT(IN) :: level
     INTEGER :: fomp_get_ancestor_thread_num
-    IF (.FALSE.) PRINT *, level
 
-    fomp_get_ancestor_thread_num = -1
+
+    fomp_get_ancestor_thread_num = omp_get_ancestor_thread_num(level)
+# 386
+
 
 END FUNCTION fomp_get_ancestor_thread_num
 
 
 FUNCTION fomp_get_team_size(level)
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER, INTENT(IN) :: level
     INTEGER :: fomp_get_team_size
-    IF (.FALSE.) PRINT *, level
 
-    fomp_get_team_size = -1
+
+    fomp_get_team_size = omp_get_team_size(level)
+# 408
+
 
 END FUNCTION fomp_get_team_size
 
 
 FUNCTION fomp_get_active_level()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER :: fomp_get_active_level
 
-    fomp_get_active_level = -1
+
+    fomp_get_active_level = omp_get_active_level()
+# 428
+
 
 END FUNCTION fomp_get_active_level
 
 
 FUNCTION fomp_in_final()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     LOGICAL :: fomp_in_final
 
-    fomp_in_final = .FALSE.
+
+    fomp_in_final = omp_in_final()
+# 448
+
 
 END FUNCTION fomp_in_final
 
 
 SUBROUTINE fomp_init_lock(svar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_lock_kind), INTENT(OUT) :: svar
 
-    svar = -1
+
+    CALL omp_init_lock(svar)
+# 469
+
 
 END SUBROUTINE fomp_init_lock
 
 
 SUBROUTINE fomp_init_nest_lock(nvar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(OUT) :: nvar
 
-    nvar = -1
+
+    CALL omp_init_nest_lock(nvar)
+# 490
+
 
 END SUBROUTINE fomp_init_nest_lock
 
 
 SUBROUTINE fomp_destroy_lock(svar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_lock_kind), INTENT(INOUT) :: svar
-    IF (.FALSE.) PRINT *, svar
+
+
+    CALL omp_destroy_lock(svar)
+# 511
+
+
 
 END SUBROUTINE fomp_destroy_lock
 
 
 SUBROUTINE fomp_destroy_nest_lock(nvar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(INOUT) :: nvar
-    IF (.FALSE.) PRINT *, nvar
+
+
+    CALL omp_destroy_nest_lock(nvar)
+# 533
+
 
 END SUBROUTINE fomp_destroy_nest_lock
 
 
 SUBROUTINE fomp_set_lock(svar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_lock_kind), INTENT(INOUT) :: svar
-    IF (.FALSE.) PRINT *, svar
+
+
+    CALL omp_set_lock(svar)
+# 554
+
 
 END SUBROUTINE fomp_set_lock
 
 
 SUBROUTINE fomp_set_nest_lock(nvar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(INOUT) :: nvar
-    IF (.FALSE.) PRINT *, nvar
+
+
+    CALL omp_set_nest_lock(nvar)
+# 575
+
 
 END SUBROUTINE fomp_set_nest_lock
 
 
 SUBROUTINE fomp_unset_lock(svar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_lock_kind), INTENT(INOUT) :: svar
-    IF (.FALSE.) PRINT *, svar
+
+
+    CALL omp_unset_lock(svar)
+# 596
+
 
 END SUBROUTINE fomp_unset_lock
 
 
 SUBROUTINE fomp_unset_nest_lock(nvar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(INOUT) :: nvar
-    IF (.FALSE.) PRINT *, nvar
+
+
+    CALL omp_unset_nest_lock(nvar)
+# 617
+
 
 END SUBROUTINE fomp_unset_nest_lock
 
 
 FUNCTION fomp_test_lock(svar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_lock_kind), INTENT(INOUT) :: svar
     LOGICAL :: fomp_test_lock
-    IF (.FALSE.) PRINT *, svar
 
-    fomp_test_lock = .FALSE.
+
+    fomp_test_lock =  omp_test_lock(svar)
+# 640
+
+
+
 
 END FUNCTION fomp_test_lock
 
 
 FUNCTION fomp_test_nest_lock(nvar)
 
+    USE omp_lib
+
     USE omp_constants, ONLY : fomp_nest_lock_kind
 
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     INTEGER(KIND=fomp_nest_lock_kind), INTENT(INOUT) :: nvar
     INTEGER :: fomp_test_nest_lock
-    IF (.FALSE.) PRINT *, nvar
 
-    fomp_test_nest_lock = -1
+
+    fomp_test_nest_lock =  omp_test_nest_lock(nvar)
+# 665
+
+
+
 
 END FUNCTION fomp_test_nest_lock
 
 
 FUNCTION fomp_get_wtime()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     REAL (KIND=8) :: fomp_get_wtime
 
-    fomp_get_wtime = -1
+
+    fomp_get_wtime =  omp_get_wtime()
+# 687
+
+
+
 
 END FUNCTION fomp_get_wtime
 
 
 FUNCTION fomp_get_wtick()
 
+    USE omp_lib
+
+
     IMPLICIT NONE
 
-    !f2py threadsafe
+!f2py threadsafe
 
     REAL (KIND=8) :: fomp_get_wtick
 
-    fomp_get_wtick = -1
+
+    fomp_get_wtick =  omp_get_wtick()
+# 709
+
+
+
 
 END FUNCTION fomp_get_wtick
 
